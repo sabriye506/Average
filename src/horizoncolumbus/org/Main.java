aed68b42bdd7b4ceb39415b46d2ffd6f1d917321
Sabriyes Project <student.sbr.mohamed7@horizoncolumbus.org>
2021-02-24 20:43:17 -0500

    Initial commit

---
+++ src/horizoncolumbus/org/Main.java
@@ -0,0 +1,15 @@
+package horizoncolumbus.org;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] unsortedArray = new int[]{5, 33, 7, 1,0, -4, 44, 12, 98};
+
+        for (int index = 0; index < unsortedArray.length; index++){
+            System.out.print(unsortedArray[index] + "\t");
+        }
+        int[] sortedArray = new int[unsortedArray.length];
+
+    }
+}
